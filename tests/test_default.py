bb0a720e2113c09268ac5a0991355bfcb65f96be
Paweł Krupa <pkrupa@sointeractive.pl>
2017-11-07 13:36:12 +0100

    add Oracle Linux installation

--- tests/test_default.py
+++ tests/test_default.py
@@ -3,53 +3,52 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_directories(File):
+def test_directories(host):
     present = [
         "/etc/docker",
     ]
     absent = []
     if present:
         for directory in present:
-            d = File(directory)
+            d = host.file(directory)
             assert d.is_directory
             assert d.exists
     if absent:
         for directory in absent:
-            d = File(directory)
+            d = host.file(directory)
             assert not d.exists
 
 
-def test_files(File):
+def test_files(host):
     present = [
         "/etc/docker/daemon.json",
     ]
     if present:
         for file in present:
-            f = File(file)
+            f = host.file(file)
             assert f.exists
             assert f.is_file
 
 
-def test_socket(Socket):
-    assert Socket("unix:///var/run/docker.sock").is_listening
+def test_socket(host):
+    assert host.socket("unix:///var/run/docker.sock").is_listening
 
 
-def test_service(Service):
+def test_service(host):
     present = [
         "docker"
     ]
     if present:
         for service in present:
-            s = Service(service)
+            s = host.service(service)
             assert s.is_running
             assert s.is_enabled
 
 
-def test_packages(Package):
-    present = [
-        "docker-ce"
-    ]
-    if present:
-        for package in present:
-            p = Package(package)
-            assert p.is_installed
+def test_packages(host):
+    if host.system_info.distribution == 'ol':
+        DOCKER = 'docker-engine'
+    else:
+        DOCKER = 'docker-ce'
+
+    assert host.package(DOCKER).is_installed
